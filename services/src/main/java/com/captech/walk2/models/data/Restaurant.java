36a1053d4a17bb26dc2d8a32c2e9ed6cf3a00aa6
Macon Pegram <mpegram@captechconsulting.com>
2013-11-21 16:23:54 -0500

    Simplified data model for now.

---
+++ services/src/main/java/com/captech/walk2/models/data/Restaurant.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.captech.walk2.models.data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * @author Macon
+ */
+@XmlAccessorType( XmlAccessType.FIELD )
+public class Restaurant {
+
+	private String name;
+	private double distance;
+	private int rating;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
+}
